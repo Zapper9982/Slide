090ec976b9629f2f66dd0cf61861515b523adf9c
Zapper9982 <144029909+Zapper9982@users.noreply.github.com>
2024-12-26 13:23:32 +0530

    LandingPage + initialDB setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import {
+  ClerkProvider,
+ 
+} from '@clerk/nextjs'
+import { ThemeProvider } from "@/providers/theme-provider";
+import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +21,25 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <ClerkProvider>
+      <html lang="en">
+     
+      <body 
+      suppressHydrationWarning 
+      className={inter.className}
+      >
+      <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >{children}
+          <Toaster />
+        </ThemeProvider>
+        </body>
+     
     </html>
+    </ClerkProvider>
+    
   );
 }
